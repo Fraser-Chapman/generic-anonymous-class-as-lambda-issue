49e1d4f4599944a8a67be88ec23284766ab137dd
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2023-08-20 14:24:25 +0100

    Initial commit of spring initializr project

---
+++ src/main/java/uk/co/fraserchapman/genericanonymousclassaslambdaissue/GenericAnonymousClassAsLambdaIssueApplication.java
@@ -0,0 +1,13 @@
+package uk.co.fraserchapman.genericanonymousclassaslambdaissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericAnonymousClassAsLambdaIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericAnonymousClassAsLambdaIssueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/fraserchapman/genericanonymousclassaslambdaissue/GenericAnonymousClassAsLambdaIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.fraserchapman.genericanonymousclassaslambdaissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericAnonymousClassAsLambdaIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
